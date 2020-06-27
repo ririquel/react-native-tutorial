0911eada2eb7e807b2f3cfe67c84f4d96ed40cd9
Ricardo Riquelme S <riquelme.iromax@gmail.com>
2020-06-26 21:35:12 -0400

    Reformat code

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {Button, View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+
 import {Counter} from './Counter';
 
 function HomeScreen({navigation}: any) {
